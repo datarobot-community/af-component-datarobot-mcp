c28419dd2c7d5f8b7e0156add74355e998304c4c
Rabih Abou Fakher <rabih.aboufakher@datarobot.com>
2025-08-08 20:17:52 +0100

    Add missing copyright

--- template/{{fastmcp_app_name}}/app/recipe/tests/integration/test_recipe_placeholder.py
+++ template/{{fastmcp_app_name}}/app/recipe/tests/integration/test_recipe_placeholder.py
@@ -1,3 +1,17 @@
+# Copyright 2025 DataRobot, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytest
 from mcp.types import CallToolResult, ListToolsResult, TextContent
 
@@ -8,7 +22,7 @@ from app.base.tests.integration.mcp_utils import integration_test_mcp_session
 class TestMCPToolsIntegration:
     """Integration tests for MCP tools."""
 
-    async def test_example(self) -> None:
+    async def test_example(self):
         """Complete integration test for RecipePlaceholder through MCP"""
 
         async with integration_test_mcp_session() as session:
